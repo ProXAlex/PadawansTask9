5587468861e2c61983d53c9fa27c5112d40120ae
Alex <alex_goreloshev@mail.ru>
2019-02-17 12:59:18 +0300

    implement GetNeighbors

--- CartesianCoordinates.cs
+++ CartesianCoordinates.cs
@@ -7,8 +7,20 @@ namespace PadawansTask9
     {
         public static List<Point> GetNeighbors(Point point, int range, params Point[] points)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if(points == null)
+                throw new ArgumentNullException();
+            if(range<=0)
+                throw new ArgumentException("Range can be only positive numbers");
+
+            List<Point> resultList = new List<Point>();
+
+            foreach (var point1 in points)
+            {
+                if(Math.Abs(point1.X - point.X) <= range && Math.Abs(point1.Y - point.Y) <= range)
+                    resultList.Add(point1);
+            }
+
+            return resultList;
         }
     }
 }
